7d149324902cc7733e6542803db9d557d395ce9d
OmarLahzy <31117612+OmarLahzy@users.noreply.github.com>
2019-06-18 12:50:24 +0200

    Login Logout

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from django.contrib.auth import views as auth_Views
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('login',auth_Views.LoginView.as_view(template_name = 'account/login.html'),name = 'login'),
+    path('logout',auth_Views.LogoutView.as_view(),name = 'logout'),
+    path('SignUp',views.SignUp.as_view(),name = 'signup')
+]
